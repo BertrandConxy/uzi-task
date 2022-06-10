07c9d029d0bf8830049b52b87f581046ece3773c
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-06-10 16:46:43 +0200

    all pages done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Payment from './pages/Payment';
 import Question from './pages/Question';
 import FinalResult from './pages/FinalResult';
 import Answers from './pages/Answers';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route path="/overview" element={<Overview />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/question" element={<Question />} />
