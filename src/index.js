cad4b4c7ac61c28044f41679284a7dcf64f17c2d
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-06-10 18:01:39 +0200

    fix warnings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './stylesheets/custom.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/index.css';
 import App from './App';
